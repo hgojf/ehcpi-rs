704da324af146d1c5dbcb6a8c575ab638b5f2f6f
Henry Ford <fordhenry2299@gmail.com>
2023-11-06 17:29:30 -0500

    always daemonize

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use ehcpi_rs::*;
+use daemonize::Daemonize;
+
+fn main() {
+	Daemonize::new()
+	.start()
+	.expect("failed to daemonize");
+	tokio_main();
+}
 
 #[tokio::main]
-async fn main() {
+async fn tokio_main() {
 	run().await;
 }
